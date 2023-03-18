174a4a0f953e2178c6477d15b6d5a46536fe41ac
Yonatan Afewerk Teshome <yonatanafewerk@gmail.com>
2023-03-18 19:16:07 +0300

    first commit

---
+++ src/main/java/com/_1894/_1894/HelloController.java
@@ -0,0 +1,14 @@
+package com._1894._1894;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com._1894._1894 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com._1894._1894 to javafx.fxml;
+    exports com._1894._1894;
+}
