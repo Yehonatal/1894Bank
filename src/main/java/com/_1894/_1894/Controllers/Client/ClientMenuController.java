28bb9ad63daca696d8657f3c642eda5e42b86b74
Yonatan Afewerk Teshome <yonatanafewerk@gmail.com>
2023-03-19 00:30:59 +0300

    Client Menu front end done

---
+++ src/main/java/com/_1894/_1894/Controllers/Client/ClientMenuController.java
@@ -0,0 +1,21 @@
+package com._1894._1894.Controllers.Client;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientMenuController implements Initializable {
+    public Button dashboard_btn;
+    public Button transcation_btn;
+    public Button accounts_btn;
+    public Button profile_btn;
+    public Button logout_btn;
+    public Button report_btn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
