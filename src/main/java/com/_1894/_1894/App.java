f398e183bcd299171e35da72cdbf804a93ca299f
Yonatan Afewerk Teshome <yonatanafewerk@gmail.com>
2023-03-18 23:04:28 +0300

    login page front end done

--- src/main/java/com/_1894/_1894/App.java
+++ src/main/java/com/_1894/_1894/App.java
@@ -1,4 +1,16 @@
 package com._1894._1894;
 
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App  extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
 }
--- src/main/java/com/_1894/_1894/Controllers/LoginController.java
+++ src/main/java/com/_1894/_1894/Controllers/LoginController.java
@@ -1,4 +1,16 @@
 package com._1894._1894.Controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    public ChoiceBox acc_selector;
+    public Label payee_address_lbl;
+    public TextField payee_address_field;
+    public Label payee_password;
+    public TextField password_field;
+    public Button login_btn;
+    public Label error_lbl;
 }
