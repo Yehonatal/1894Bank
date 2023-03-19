9db828284cb86e754c0c8a0f97a5744c196c2809
Yonatan Afewerk Teshome <yonatanafewerk@gmail.com>
2023-03-19 03:34:09 +0300

    Client Dashboard front end done

--- src/main/java/com/_1894/_1894/App.java
+++ src/main/java/com/_1894/_1894/App.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 public class App  extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/Client/Dashboard.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
