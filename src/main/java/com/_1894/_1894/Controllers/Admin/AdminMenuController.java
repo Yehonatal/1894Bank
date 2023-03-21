044774cc9ee7db7bf88e144cf1249d3ec704a694
Yonatan Afewerk Teshome <yonatanafewerk@gmail.com>
2023-03-21 11:40:02 +0300

    Admin menu set up :0

---
+++ src/main/java/com/_1894/_1894/Controllers/Admin/AdminMenuController.java
@@ -0,0 +1,19 @@
+package com._1894._1894.Controllers.Admin;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdminMenuController implements Initializable {
+    public Button create_client_btn;
+    public Button clients_btn;
+    public Button deposit_btn;
+    public Button logout_btn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
